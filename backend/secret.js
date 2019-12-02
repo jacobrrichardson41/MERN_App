9ed9c9cb81569717240f32805ce84fac7a09bed6
Jacob Richardson <jacobrrichardson41@gmail.com>
2019-12-02 13:25:13 -0600

    Added in gitignore to ignore node modules, installed packages for server

--- backend/secret.js
+++ backend/secret.js
@@ -2,7 +2,7 @@ import { MongooseDocument } from "mongoose";
 
 const secrets = {
 //The URL that we use to connect to the MongoDB Atlas Cluster
-    dburi: 'mongodb+srv://jacob:<password>@cluster0-kzxka.mongodb.net/test?retryWrites=true&w=majority'
+    dburi: 'mongodb+srv://jacob:<123password>@cluster0-kzxka.mongodb.net/test?retryWrites=true&w=majority'
 };
 
 const getSecret = key => secrets[keys];
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,13 @@
+//Mongoose- Allows us to set-up schemas for the data we store in our MongoDB database.We will also set up our database connection using Mongoose. 
 const mongoose = require('mongoose');
+//Express-framework for NodeJS. Handles our routing and connection info (ports, etc).
 const express = require('express');
+//Morgan- Handles logging stuff for database. 
 const logger = require ('logger');
+//Body-Parser- Makes sure the body of our requests is formatted correctly (in this case, we'll use JSON)
+const bodyParser = require('body-parser');
+
+
+
+
+
